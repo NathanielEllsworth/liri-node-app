eaf68a2e158ad81f2d2779ea31d10ce6bf231bf0
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-17 14:28:58 -0400

    running through thursday's class recording, testing js file connections in node

--- liri.js
+++ liri.js
@@ -1,8 +1,22 @@
-var request = require("request");
+var theKeys = require("./keys.js");
 
+
+
+
+//just twitter
+
+console.log("-------------------------");
+console.log("Just Twitter keys");
+console.log(theKeys.twitter);
+console.log("-------------------------");
+
+
+
+
+/*
 var params = {screen_name: 'nodejs'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
         console.log(tweets);
     }
-});
+});*/
