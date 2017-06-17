5e4bfda48805b2f866764c66c52c566d3e098f07
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-17 19:05:50 -0400

    messing around, quick save

--- keys.js
+++ keys.js
@@ -1,17 +1,20 @@
+var fs = require("fs");
 
 
-exports.twitter = {
+
+
+/*exports.twitter = {
 
     consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
     consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
     access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
     access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
 
-};
+};*/
 
 
 
-var Twitter = require('twitter');
+//var Twitter = require('twitter');
 
 var client = new Twitter({
     consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
@@ -20,9 +23,15 @@ var client = new Twitter({
     access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
 });
 
+module.exports = new client.allow_promise(Twitter);
+
+
+
 var params = {screen_name: 'SwimBikeRun1'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
         console.log(tweets);
     }
 });
+
+module.exports = params;
--- liri.js
+++ liri.js
@@ -1,16 +1,38 @@
+var fs = require("fs");
+
 var theKeys = require("./keys.js");
 
+//asynchronously reads the entire contents of a file. Example:
+
+fs.readFile('keys.js', (err, data) => {
+    if (err) throw err;
+console.log(data);
+});
 
 
+//if options is a string, then it specifies the encoding. Example:
+// fs.readFile(file[,options],callback)
+
+fs.readFile('keys.js', 'utf8', callback); //callback is (err, data)
 
 //just twitter
 
 console.log("-------------------------");
+
 console.log("Just Twitter keys");
-console.log(theKeys.twitter);
+console.log('twitter', theKeys.Twitter);
+console.log('twitter', theKeys.consumer_key);
+console.log('twitter', theKeys.consumer_secret);
+console.log('twitter', theKeys.access_token_key);
+console.log('twitter', theKeys.access_token_secret);
+
 console.log("-------------------------");
 
 
+
+//login
+
+console.log("params", theKeys.screen_name);
 
 
 /*
@@ -20,3 +42,4 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         console.log(tweets);
     }
 });*/
+
