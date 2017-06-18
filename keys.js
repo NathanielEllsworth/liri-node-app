378099b7068cb1ff5f4bd2938a5f8899ef85e76b
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-18 14:34:53 -0400

    pulling tweets

--- keys.js
+++ keys.js
@@ -1,20 +1,39 @@
-var fs = require("fs");
+/*
+var twit = require('twitter'),
+    twitter = new twit({
+        consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
+        consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
+        access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
+        access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
+
+    });
+
+var count = 0;
+    util = require('util');
+
+twitter.stream('filter', {track: 'test'}, function(stream){
+
+        stream.on('data',function(data){
+            console.log(util.inspect(data));
+            stream.destroy();
+            process.exit(0);
+
+        });
+});*/
 
 
-
-
-/*exports.twitter = {
-
+/*exports.twitterKeys = {
     consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
     consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
     access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
     access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
-
 };*/
 
 
 
-//var Twitter = require('twitter');
+
+
+var Twitter = require('twitter');
 
 var client = new Twitter({
     consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
@@ -23,15 +42,21 @@ var client = new Twitter({
     access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
 });
 
-module.exports = new client.allow_promise(Twitter);
 
 
 
-var params = {screen_name: 'SwimBikeRun1'};
+
+var params = {screen_name: 'SwimRunBike1'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
+    if (error) throw error;
+
+    console.log(tweets[0].user.name);
+
+    for (var i = 0; i < 4; i++) {
+
+        console.log("tweet -------- ", tweets[i].text);
+
+        console.log("time tweeted ------------ ", tweets[i].created_at);
     }
 });
 
-module.exports = params;
