c0ab8d33133ced2e13b45c3ce5acbf15e55388ea
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-18 18:40:43 -0400

    pulling .txt file and injecting it into spotify

--- keys.js
+++ keys.js
@@ -1,62 +1,72 @@
 /*
-var twit = require('twitter'),
-    twitter = new twit({
-        consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
-        consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
-        access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
-        access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
+ var twit = require('twitter'),
+ twitter = new twit({
+ consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
+ consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
+ access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
+ access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
 
-    });
+ });
 
-var count = 0;
-    util = require('util');
+ var count = 0;
+ util = require('util');
 
-twitter.stream('filter', {track: 'test'}, function(stream){
+ twitter.stream('filter', {track: 'test'}, function(stream){
 
-        stream.on('data',function(data){
-            console.log(util.inspect(data));
-            stream.destroy();
-            process.exit(0);
+ stream.on('data',function(data){
+ console.log(util.inspect(data));
+ stream.destroy();
+ process.exit(0);
 
-        });
-});*/
+ });
+ });*/
 
+console.log("200");
 
-/*exports.twitterKeys = {
+exports.twitterKeys = {
     consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
     consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
     access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
     access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
-};*/
+};
+
+//node-spotify-api
+exports.spotifyKeys = {
+    client_id:'631db228381d4d51ab6671029849b07c',
+    client_secret:'9fdb9a0d86e244b99a394c6fb45cefe3'
+
+
+};
+
+/*
+ var Twitter = require('twitter');
+
+ var client = new Twitter({
+ consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
+ consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
+ access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
+ access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
+ });
+ */
 
 
 
 
 
-var Twitter = require('twitter');
+/*var params = {screen_name: 'SwimRunBike1'}; //user name BikeRunSwim
+ client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (error) throw error;
 
-var client = new Twitter({
-    consumer_key: 'v3dKodtJmJ7Mjwiu8w2Q4r04H',
-    consumer_secret: 'Ri6GEnITtmQwdHm6KExwLG0satO4bIULjI5l0469Ds6gXci8uW',
-    access_token_key: '875703628534878208-TDlNtANXXeMkCJC0sgwmpIiv3xIQGaf',
-    access_token_secret: 'VDdm8cgpDKUOfj1nam8qqjc32nXgmHDMtvtSGR7bSq0jV'
-});
+ console.log(tweets[0].user.name);
 
+ for (var i = 0; i < 4; i++) {
 
+ console.log("");
+ console.log("tweet -------- ", tweets[i].text);
 
+ console.log("time tweeted ------------ ", tweets[i].created_at);
+ console.log("");
+ }
+ });*/
 
-
-var params = {screen_name: 'SwimRunBike1'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (error) throw error;
-
-    console.log(tweets[0].user.name);
-
-    for (var i = 0; i < 4; i++) {
-
-        console.log("tweet -------- ", tweets[i].text);
-
-        console.log("time tweeted ------------ ", tweets[i].created_at);
-    }
-});
 
--- liri.js
+++ liri.js
@@ -3,28 +3,38 @@ var fs = require("fs");
 var keysNeeded = require("./keys.js");
 
 var twitter = keysNeeded.twitterKeys;
+var spotify = keysNeeded.spotifyKeys;
 
 var User = process.argv[2];
+
+var Text = process.argv[3];
+
 //asynchronously reads the entire contents of a file. Example:
 
 /*fs.readFile('keys.js', (err, data) => {
-    if (err) throw err;
-console.log(data);
-});*/
+ if (err) throw err;
+ console.log(data);
+ });*/
 
 
-switch (User) {
-    case 'tweets':    //type: 'node liri tweets' in the command line to get the tweets
-        getTweets();
+switch (User) {   // classwork 5.4 #15 bank.js
+    /*
+     case 'tweets':    //type: 'node liri tweets' in the command line to get the tweets
+     getTweets();
+     break;
+     */
+    case 'spotify':   //type: 'node liri spotify' in the command line to get the song
+        getSpotify();
         break;
-//    case 'songify':
-//        getSongify();
-//        break;
-//    case 'movie':
-//        getMovie();
-//        break;
+    case 'movie':
+        getMovie();
+        break;
+    case 'do-what-it-says':
+        getText();
+        break;
 
 
+/*
     function getTweets() {
 
 
@@ -40,7 +50,7 @@ switch (User) {
         });
 
         var params = {screen_name: 'SwimRunBike1'}; // user name: RunBikeSwim
-        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        client.get('statuses/user_timeline', params, (error, tweets, response) => {
             if (error) throw error;
 
             console.log(tweets[0].user.name);
@@ -55,12 +65,59 @@ switch (User) {
             }
         });
     }
+*/
+
+
+    function getSpotify() {
+
+        var Spotify = require('node-spotify-api');      //https://www.npmjs.com/package/node-spotify-api
+
+        var client = new Spotify({
+            id: spotify.client_id,
+            secret: spotify.client_secret
+
+        });
+
+        client.search({type: 'track', query: 'All the Small Things'}, (error, data) => {   //api site
+            if (error) {
+                return console.log('error occured: ' + error);
+            }
+
+            console.log(data);
+        })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+    }
+
+    function getText() {
+        fs.readFile("random.txt", "utf8", (error, data) => {    //Classwork 5.4 #13
+            if (error) {
+                return console.log(error);
+            }
+
+            var output = data.slice(0);       //stack overflow
+
+            console.log(output[0]);
+
+            if (output[0] === User) {
+
+                Text = output[1]
+
+
+                getSpotify()
+            }
+
+        })
+
+
+    }
 
 }
-
-
-
-
 
 
 //if options is a string, then it specifies the encoding. Example:
@@ -68,33 +125,33 @@ switch (User) {
 
 /*fs.readFile('keys.js', 'utf8', callback); //callback is (err, data)
 
-//just twitter
+ //just twitter
 
-console.log("-------------------------");
+ console.log("-------------------------");
 
-console.log("Just Twitter keys");
-console.log('twitter', theKeys.Twitter);
-console.log('twitter', theKeys.consumer_key);
-console.log('twitter', theKeys.consumer_secret);
-console.log('twitter', theKeys.access_token_key);
-console.log('twitter', theKeys.access_token_secret);
+ console.log("Just Twitter keys");
+ console.log('twitter', theKeys.Twitter);
+ console.log('twitter', theKeys.consumer_key);
+ console.log('twitter', theKeys.consumer_secret);
+ console.log('twitter', theKeys.access_token_key);
+ console.log('twitter', theKeys.access_token_secret);
 
-console.log("-------------------------");
+ console.log("-------------------------");
 
 
 
-//login
+ //login
 
-console.log("params", theKeys.screen_name);*/
+ console.log("params", theKeys.screen_name);*/
 
 
 /*
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
-    }
-});*/
+ var params = {screen_name: 'nodejs'};
+ client.get('statuses/user_timeline', params, function(error, tweets, response) {
+ if (!error) {
+ console.log(tweets);
+ }
+ });*/
 
 
 
