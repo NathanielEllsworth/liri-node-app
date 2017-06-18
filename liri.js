891b4767139c8e25713f0c8aced6cc70aa8871e9
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-06-18 16:30:43 -0400

    added switch statements, working on songify

--- liri.js
+++ liri.js
@@ -1,19 +1,72 @@
 var fs = require("fs");
 
-var theKeys = require("./keys.js");
+var keysNeeded = require("./keys.js");
 
+var twitter = keysNeeded.twitterKeys;
+
+var User = process.argv[2];
 //asynchronously reads the entire contents of a file. Example:
 
-fs.readFile('keys.js', (err, data) => {
+/*fs.readFile('keys.js', (err, data) => {
     if (err) throw err;
 console.log(data);
-});
+});*/
+
+
+switch (User) {
+    case 'tweets':    //type: 'node liri tweets' in the command line to get the tweets
+        getTweets();
+        break;
+//    case 'songify':
+//        getSongify();
+//        break;
+//    case 'movie':
+//        getMovie();
+//        break;
+
+
+    function getTweets() {
+
+
+        var Twitter = require('twitter');
+
+        var client = new Twitter({
+            consumer_key: twitter.consumer_key,
+            consumer_secret: twitter.consumer_secret,
+            access_token_key: twitter.access_token_key,
+            access_token_secret: twitter.access_token_secret
+
+
+        });
+
+        var params = {screen_name: 'SwimRunBike1'}; // user name: RunBikeSwim
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (error) throw error;
+
+            console.log(tweets[0].user.name);
+
+            for (var i = 0; i < 4; i++) {
+
+                console.log("");
+                console.log("tweet -------- ", tweets[i].text);
+
+                console.log("time tweeted ------------ ", tweets[i].created_at);
+                console.log("");
+            }
+        });
+    }
+
+}
+
+
+
+
 
 
 //if options is a string, then it specifies the encoding. Example:
 // fs.readFile(file[,options],callback)
 
-fs.readFile('keys.js', 'utf8', callback); //callback is (err, data)
+/*fs.readFile('keys.js', 'utf8', callback); //callback is (err, data)
 
 //just twitter
 
@@ -32,7 +85,7 @@ console.log("-------------------------");
 
 //login
 
-console.log("params", theKeys.screen_name);
+console.log("params", theKeys.screen_name);*/
 
 
 /*
@@ -42,4 +95,6 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
         console.log(tweets);
     }
 });*/
+
+
 
